1f621629f1e5f2ea318533f4a5505f50982283b5
cutofmyjib <thedianalozano@gmail.com>
2015-12-08 00:19:24 -0800

    import css, font and img to test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { App } from './App';
+import '../static/style.css';
+import '../static/ClearSans-Bold.ttf';
+import '../static/dancing_drakes.jpg';
 
 render(<App />, document.getElementById('root'));
