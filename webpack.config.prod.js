7547ab1206feec6e48f68e4d5495a579456f0d30
cutofmyjib <thedianalozano@gmail.com>
2015-12-07 20:35:29 -0800

    file-loader for images

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -29,6 +29,17 @@ module.exports = {
       test: /\.js$/,
       loaders: ['babel'],
       include: path.join(__dirname, 'src')
+    },
+    //https://github.com/petehunt/webpack-howto
+    {
+      test: /\.css$/,
+      loader: 'style-loader!css-loader'
+    },
+    // { test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+    //   loader: 'url-loader?limit=100000'
+    // }
+    { test: /\.(png|jpg)$/,
+      loader: 'file-loader?name=[name].[ext]'
     }]
   }
 };
