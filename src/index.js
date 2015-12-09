50c50bdbb06759015a15aa1c80a683df343e8c2f
cutofmyjib <thedianalozano@gmail.com>
2015-12-09 01:09:42 -0800

    import images

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import { App } from './App';
 import '../static/style.css';
 import '../static/ClearSans-Bold.ttf';
 import '../static/dancing_drakes.jpg';
+import '../static/pusheen_the_cats.png';
+import '../static/hipster_floral_triangles.jpg';
 
 render(<App />, document.getElementById('main'));
