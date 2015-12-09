d0613ddfebd6d05d81e5df9bb497573b62cdf7b4
cutofmyjib <thedianalozano@gmail.com>
2015-12-09 01:59:45 -0800

    import image

--- src/index.js
+++ src/index.js
@@ -6,5 +6,6 @@ import '../static/ClearSans-Bold.ttf';
 import '../static/dancing_drakes.jpg';
 import '../static/pusheen_the_cats.png';
 import '../static/hipster_floral_triangles.jpg';
+import '../static/floral_tea_cups.gif';
 
 render(<App />, document.getElementById('main'));
