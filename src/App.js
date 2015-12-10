9125bff51a4afcb7b3f6d050e01bff0f06710ec9
cutofmyjib <thedianalozano@gmail.com>
2015-12-09 21:32:37 -0800

    created Header class component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
 import React, { Component } from 'react';
 
-// class Test extends Component {
-//   render() {
-//     return (
-//       <h1>poop</h1>
-//     );
-//   }
-// }
+class Header extends Component {
+  render() {
+    return (
+      <header className="portfolio-header row-stack">
+        <h1 className="portfolio-title">{this.props.title}</h1>
+        <nav className="portfolio-nav">
+          <ul className="portfolio-menu">
+            <li className="active"><a href="#">GALLERY</a></li>
+            <li><a href="#">BLOG</a></li>
+            <li><a href="#">ABOUT</a></li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+}
 
-// export class App extends Component {
-//   render() {
-//     return (
 
-//     );
-//   }
-// }
+class Gallery extends Component {
+  render() {
+    return (
+
+    );
+  }
+}
+
+
+
+
+
+export class App extends Component {
+  render() {
+    var options = {
+      title: "MY PORTFOLIO"
+    };
+
+    return (
+      <Header {...options}/>
+    );
+  }
+}
