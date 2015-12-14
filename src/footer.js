c07a9cf23d8c74c71223c1932134715e11d0430c
cutofmyjib <thedianalozano@gmail.com>
2015-12-13 23:36:49 -0800

    move Footer component to new js file

---
+++ src/footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Footer extends Component {
+  render(){
+    return (
+      <footer>
+        <ul className="footer-menu">
+          <li className="linkedin"><a href="#"></a></li>
+          <li className="twitter"><a href="#"></a></li>
+          <li className="email"><a href="#"></a></li>
+        </ul>
+      </footer>
+    );
+  }
+}
