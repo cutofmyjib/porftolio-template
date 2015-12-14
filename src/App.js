c1a2ef2e712f1787c2a65d9e7e1f38aa0827cc24
cutofmyjib <thedianalozano@gmail.com>
2015-12-13 23:22:37 -0800

    take out Header component from App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
 import React, { Component } from 'react';
+import Header from './header';
 
-class Header extends Component {
-  render() {
-    return (
-      <header className="portfolio-header row-stack">
-        <h1 className="portfolio-title">{this.props.title}</h1>
-        <nav className="portfolio-nav">
-          <ul className="portfolio-menu">
-            <li className="active"><a href="#">GALLERY</a></li>
-            <li><a href="#">BLOG</a></li>
-            <li><a href="#">ABOUT</a></li>
-          </ul>
-        </nav>
-      </header>
-    );
-  }
-}
 
 class Gallery extends Component {
   render() {
