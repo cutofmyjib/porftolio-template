8181c406aba6f720f1c5f10182cbf6870d7ea6f4
cutofmyjib <thedianalozano@gmail.com>
2015-12-17 14:45:44 -0800

    add history.js

---
+++ src/history.js
@@ -0,0 +1,4 @@
+// https://github.com/rackt/react-router/blob/latest/docs/guides/advanced/NavigatingOutsideOfComponents.md
+
+import createBrowserHistory from 'history/lib/createBrowserHistory'
+export default createBrowserHistory()
